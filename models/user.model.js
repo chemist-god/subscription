16ea265f3ca2753ebdb7b1e61d82d1fe3cb5ebba
chemist-god <godtenkorang72@gmail.com>
2025-02-13 23:53:13 +0000

    add email field to user schema with validation rules

--- models/user.model.js
+++ models/user.model.js
@@ -10,4 +10,13 @@ const userSchema = new mongoose.Schema({
         minLength: [3, 'User Name must be at least 3 characters long'],
         maxLength: [50, 'User Name must be at most 50 characters long']
     },
+    email: {
+        type: String,
+        required: [true, 'User Email is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'User Email is invalid'],
+        maxLength: [20, 'User Email must be at most 20 characters long'],
+    },
 });
