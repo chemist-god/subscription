58469ec3a89b900f3fc82249d862c0e5283e7393
chemist-god <godtenkorang72@gmail.com>
2025-02-13 23:58:38 +0000

    add name field to subscription schema with validation rules

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -1,4 +1,13 @@
 import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: [true, 'Subscription Name is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: [3, 'Subscription Name must be at least 3 characters long'],
+        maxLength: [50, 'Subscription Name must be at most 50 characters long']
+    },
 } , { timestamps: true });
