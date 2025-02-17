354ee4e6dc14c475f58a8f1f4ca44b869c408328
chemist-god <godtenkorang72@gmail.com>
2025-02-17 22:42:44 +0000

    add cookie parser middleware and extend environment variables for JWT configuration

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
