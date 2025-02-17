e4d0908fb8c14d25e21ec1e062f4bcd3da4c0aed
chemist-god <godtenkorang72@gmail.com>
2025-02-17 22:09:34 +0000

    handle Mongoose duplicate key errors

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -12,6 +12,14 @@ const errorMiddleware = (err, req, res, next) => {
             error = new Error(message);
             error.statusCode = 404;
         }
+
+        // Mongoose duplicate key
+        else if (err.code === 11000) {
+            const message = 'Duplicate field value entered';
+
+            error = new Error(message);
+            error.statusCode = 400;
+        }
         
     } catch (error) {
         next(error);
