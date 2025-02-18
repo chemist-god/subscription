4fafe7cbda222f315343186c03cd789229634d88
chemist-god <godtenkorang72@gmail.com>
2025-02-18 23:17:22 +0000

    add authentication controller with signUp, signIn, and signOut methods

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signUp = async (req, res) => {
+    //Implement signup logic here
+}
+
+export const signIn = async (req, res) => {
+    //Implement sign in logic here
+}
+
+export const signOut = async (req, res) => {
+    //Implement signout logic here
+}
