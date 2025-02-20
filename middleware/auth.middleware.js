6cf8392703d1561fa43a60d98d68d6299fb25abd
chemist-god <godtenkorang72@gmail.com>
2025-02-20 00:13:22 +0000

    add authorization middleware to verify JWT and user existence

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+
+
+const authorize = (req, res, next) => {
+    try {
+        let token;
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            
+            const user = await User.findById(decoded.userId);
+            if (!user) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+            req.user = user;
+        }
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
